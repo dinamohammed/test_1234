e7efdb50f23e14f42a87fea462741d488f35ce3e
dinamohammed <dina.mohammed392@gmail.com>
2019-11-24 11:29:16 +0000

    credit_limit

--- credit_limit/models/models.py
+++ credit_limit/models/models.py
@@ -5,4 +5,15 @@ from odoo import models, fields, api
 class saleorder(models.Model):
     _inherit='sale.order'
     
-    credit_limit = fields.Boolean(string="Credit Limit")
+    credit_limit = fields.Boolean(string="Credit Limit" , compute='_compute_credit_limit')
+    
+  #  @api.depends('credit_limit,credit_limit_value')
+   # def _compute_credit_limit(self):
+    #    for record in self :
+     #       record[('credit_limit')]=record.partner_invoice_id.credit > record.partner_invoice_id.credit_limit_value
+            
+            
+class resPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    credit_limit_value = fields.Float(string="Credit Limit Value")
