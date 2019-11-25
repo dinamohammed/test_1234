a27e6d6d659184f076c574ff531e4d140cd6b991
dinamohammed <dina.mohammed392@gmail.com>
2019-11-25 14:34:13 +0000

    credit_limit

--- credit_limit/models/.ipynb_checkpoints/models-checkpoint.py
+++ credit_limit/models/.ipynb_checkpoints/models-checkpoint.py
@@ -8,13 +8,13 @@ class saleorder(models.Model):
     credit_limit = fields.Boolean(string="Credit Limit")
     
     # compute='_compute_credit_limit'
-  #  @api.depends('credit_limit,credit_limit_value')
-   # def _compute_credit_limit(self):
-    #    for record in self :
-     #       record[('credit_limit')]=record.partner_invoice_id.credit > record.partner_id.total_due
+    #@api.depends('credit_limit,credit_limit_value','due')
+  #  def _compute_credit_limit(self):
+   #     for record in self :
+    #record[('credit_limit')]=record.partner_invoice_id.credit > record.partner_id.total_due
             
             
 class resPartner(models.Model):
     _inherit = 'res.partner'
     
-    credit_limit_value = fields.Float(string="Credit Limit Value")
+    #credit_limit_value = fields.Float(string="Credit Limit Value")
