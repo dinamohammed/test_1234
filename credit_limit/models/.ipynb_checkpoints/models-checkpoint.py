3f979164a23b8bf86c4109729585f0502867a1a1
dinamohammed <dina.mohammed392@gmail.com>
2019-11-28 11:10:28 +0000

    credit_limit

--- credit_limit/models/.ipynb_checkpoints/models-checkpoint.py
+++ credit_limit/models/.ipynb_checkpoints/models-checkpoint.py
@@ -5,12 +5,13 @@ from odoo import models, fields, api
 class saleorder(models.Model):
     _inherit='sale.order'
     
-    credit_limit_bool = fields.Boolean(string="Credit Limit" , compute='_compute_credit_limit' , store=True)
+    credit_limit_bool = fields.Boolean(string="Over Credit" , compute='_compute_credit_limit')
     
     
     @api.depends('partner_invoice_id.credit_limit' ,'partner_invoice_id.credit')
     def _compute_credit_limit(self):
          for record in self :
-             record[('credit_limit_bool')] = record.partner_invoice_id.credit > record.partner_invoice_id.credit_limit
-              
-            
+                if record.amount_total > record.partner_invoice_id.credit_limit :
+                        record[('credit_limit_bool')] = True   
+                elif record.partner_invoice_id.credit > record.partner_invoice_id.credit_limit :
+                        record[('credit_limit_bool')] = True
