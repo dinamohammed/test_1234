0893017b0521b37782d2bb74620f42d65f97adc5
dinamohammed <dina.mohammed392@gmail.com>
2020-01-19 14:54:47 +0000

    Accounting date

--- force_date/models/models.py
+++ force_date/models/models.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 
-class stockinventory(models.Model):
+class StockInventory(models.Model):
     
     _inherit = "stock.inventory"
     
@@ -12,3 +12,22 @@ class stockinventory(models.Model):
         default=fields.Datetime.now,
         help="If the inventory adjustment is not validated, date at which the theoritical quantities have been checked.\n"
              "If the inventory adjustment is validated, date at which the inventory adjustment has been validated.")
+    
+    def action_start(self):
+        for inventory in self.filtered(lambda x: x.state not in ('done','cancel')):
+            vals = {'state': 'confirm', 'date': inventory.date}
+            if (inventory.filter != 'partial') and not inventory.line_ids:
+                vals.update({'line_ids': [(0, 0, line_values) for line_values in inventory._get_inventory_lines_values()]})
+            inventory.write(vals)
+        return True
+    
+    
+    @api.multi
+    def post_inventory(self):
+        acc_inventories = self.filtered(lambda inventory: self.date)
+        for inventory in acc_inventories:
+            res = super(StockInventory, inventory.with_context(force_period_date=inventory.date)).post_inventory()
+        other_inventories = self - acc_inventories
+        if other_inventories:
+            res = super(StockInventory, other_inventories).post_inventory()
+        return res
